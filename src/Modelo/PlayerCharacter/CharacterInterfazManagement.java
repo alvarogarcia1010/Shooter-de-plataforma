56d7ee99e8a16da5c7502dbd4194857caf606590
alvarogarcia1010 <00111317@uca.edu.sv>
2018-06-30 20:56:06 -0600

    Init proyect

---
+++ src/Modelo/PlayerCharacter/CharacterInterfazManagement.java
@@ -0,0 +1,10 @@
+
+package Modelo.PlayerCharacter;
+
+/**
+ *
+ * @author Alvaro García <alvarogarcia1010 at github.com>
+ */
+public interface CharacterInterfazManagement {
+    
+}
--- src/PlataformShooter/PlataformShooter.java
+++ src/PlataformShooter/PlataformShooter.java
@@ -1,4 +1,4 @@
-package plataform.shooter;
+package PlataformShooter;
 
 /**
  *
