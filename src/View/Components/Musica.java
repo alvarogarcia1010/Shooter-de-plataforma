2f6bf7750c04b296e35d0b8af9555f061a5902a2
Dalcoleas <00048015@uca.edu.sv>
2018-07-09 00:26:47 -0600

    Imagenes

--- src/View/Components/Musica.java
+++ src/View/Components/Musica.java
@@ -5,10 +5,38 @@
  */
 package View.Components;
 
-/**
- *
- * @author garcia
- */
+import java.io.FileInputStream;
+import java.io.InputStream;
+import javax.swing.JOptionPane;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+
 public class Musica {
+    public static AudioStream audio1;
+    
+        
+    public void musica(){
+    
+        try{
+            String sonido1 = "./src/Music/Music.wav";
+            InputStream in = new FileInputStream(sonido1);
+            audio1 = new AudioStream(in);
+            AudioPlayer.player.start(audio1);
+            
+            
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error");
+        }
+    }
+
+    /*public  void stop(){
+        try{
+            AudioPlayer.player.stop(audio1);
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error");
+        }
+    }*/
+    
     
 }
